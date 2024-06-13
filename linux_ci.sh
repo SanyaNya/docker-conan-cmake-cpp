6e21be23a2ede93d3f5d8c6435431d3c41685c49
SanyaNya <sanyanya4ever@gmail.com>
2024-06-13 18:53:34 +0300

    Push images to dockerhub

--- linux_ci.sh
+++ linux_ci.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 cd centos7
+
 ./build32.sh
 ./build64.sh
+
+docker tag centos7_32 sanyanya/centos7_32
+docker tag centos7_64 sanyanya/centos7_64
+
+docker push sanyanya/centos7_32
+docker push sanyanya/centos7_64
--- windows_ci.bat
+++ windows_ci.bat
@@ -1,2 +1,4 @@
 cd windows
 .\build.bat
+docker tag windows-conan-cmake-cpp sanyanya/windows-conan-cmake-cpp
+docker push sanyanya/windows-conan-cmake-cpp
